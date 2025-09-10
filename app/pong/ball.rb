e4c30274cd3104e49f94b12aa48ebf7d21e2c077
Vinny Diehl <vinny.diehl@gmail.com>
2025-09-10 01:19:01 -0700

    Move the ball and handle top/bottom wall collisions

--- app/pong/ball.rb
+++ app/pong/ball.rb
@@ -2,11 +2,34 @@ class Ball
   attr_reader :position
 
   def initialize(screen_width, screen_height)
+    @screen_width, @screen_height = screen_width, screen_height
+    # Top Y value for the ball
+    @screen_top = @screen_height - BALL_DIAMETER
+
     @start_position = [screen_width / 2, screen_height / 2]
     reset
   end
 
   def reset
     @position = @start_position.map { |n| n - (BALL_DIAMETER / 2) }
+    @speed = BALL_START_SPEED
+    @angle = -80
+  end
+
+  def tick
+    # Move the ball
+    radians = @angle * Math::PI / 180.0
+    @position.x += @speed * Math.cos(radians)
+    @position.y += @speed * Math.sin(radians)
+
+    handle_wall_collision
+  end
+
+  # Bounce the ball off the top/bottom walls
+  def handle_wall_collision
+    if @position.y <= 0 || @position.y >= @screen_top
+      @angle *= -1
+      @position.y = @position.y.clamp(0, @screen_top)
+    end
   end
 end
--- app/pong/scenes/game.rb
+++ app/pong/scenes/game.rb
@@ -1,8 +1,9 @@
 class PongGame
   def game_init
     @ball = Ball.new(@screen_width, @screen_height)
-    render_game_init
   end
 
-  def game_tick; end
+  def game_tick
+    @ball.tick
+  end
 end
